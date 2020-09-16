f6955d409ef971e20880e3e55ea9848533641ca9
nag-tamaki <tamaki@nagisalabs.org>
2020-09-16 19:48:53 +0900

    test

--- trial-1/src/main/java/com/example/trial/HelloController.java
+++ trial-1/src/main/java/com/example/trial/HelloController.java
@@ -1,13 +1,38 @@
 package com.example.trial;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "nekoneko";
+	String[] name = {"orion","artemis","uranos","herios","iris"};
+	String[] mails = {"orion@star.gmobb.jp","artemis@star.gmobb.jp","uranos@star.gmobb.jp","herios@star.gmobb.jp","iris@star.gmobb.jp"};
+	@RequestMapping("/{id}")
+	public DataObject index(@PathVariable int id) {
+		return new DataObject(id,name[id],mails[id]);
+//		return "nekoneko";
 	}
+}
+
+class DataObject{
+	private int id;
+	private String name;
+	private String value;
+
+	public DataObject(int id, String name, String value) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.value = value;
+	}
+
+
+public int getId() {return id; }
+public void setId(int id) {this.id = id; }
+public String getName() {return name;}
+public void setName(String name) {this.name = name;}
+public String getValue() {return value;}
+public void setValue(String value) {this.value = value;}
 }
