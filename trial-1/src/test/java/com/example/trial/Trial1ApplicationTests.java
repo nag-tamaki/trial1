0d6316f8933a71499dcd555f996f9562a28cd966
nag-tamaki <tamaki@nagisalabs.org>
2020-09-16 14:37:05 +0900

    new1

---
+++ trial-1/src/test/java/com/example/trial/Trial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
