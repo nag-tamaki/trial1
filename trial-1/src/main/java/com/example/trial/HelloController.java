9dd471e05892037f1f8c76d9489c448c01fc8bde
nag-tamaki <tamaki@nagisalabs.org>
2020-09-17 00:07:02 +0900

    5

--- trial-1/src/main/java/com/example/trial/HelloController.java
+++ trial-1/src/main/java/com/example/trial/HelloController.java
@@ -1,19 +1,27 @@
 package com.example.trial;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num, Model model) {
-		int res = 0;
-		for(int i=1; i<=num; i++)
-			res += i;
-		model.addAttribute("msg", "total:" + res);
-		return "index";
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "please write your name");
+		return mav;
+	}
+
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1")String str,
+			ModelAndView mav) {
+		mav.addObject("msg","hello " + str );
+		mav.addObject("value",str);
+		mav.setViewName("index");
+		return mav;
 	}
 }
