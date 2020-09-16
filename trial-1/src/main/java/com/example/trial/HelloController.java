0d1a56a941ff5452577ad6dae43a402688958ae6
nag-tamaki <tamaki@nagisalabs.org>
2020-09-16 21:17:59 +0900

    4

--- trial-1/src/main/java/com/example/trial/HelloController.java
+++ trial-1/src/main/java/com/example/trial/HelloController.java
@@ -1,38 +1,19 @@
 package com.example.trial;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-	String[] name = {"orion","artemis","uranos","herios","iris"};
-	String[] mails = {"orion@star.gmobb.jp","artemis@star.gmobb.jp","uranos@star.gmobb.jp","herios@star.gmobb.jp","iris@star.gmobb.jp"};
-	@RequestMapping("/{id}")
-	public DataObject index(@PathVariable int id) {
-		return new DataObject(id,name[id],mails[id]);
-//		return "nekoneko";
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		int res = 0;
+		for(int i=1; i<=num; i++)
+			res += i;
+		model.addAttribute("msg", "total:" + res);
+		return "index";
 	}
-}
-
-class DataObject{
-	private int id;
-	private String name;
-	private String value;
-
-	public DataObject(int id, String name, String value) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.value = value;
-	}
-
-
-public int getId() {return id; }
-public void setId(int id) {this.id = id; }
-public String getName() {return name;}
-public void setName(String name) {this.name = name;}
-public String getValue() {return value;}
-public void setValue(String value) {this.value = value;}
 }
