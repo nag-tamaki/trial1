ccd5acb91459eec845a4f9234b063a2cd10b1a94
nag-tamaki <tamaki@nagisalabs.org>
2020-09-17 15:00:30 +0900

    3.5

--- trial-1/src/main/java/com/example/trial/HelloController.java
+++ trial-1/src/main/java/com/example/trial/HelloController.java
@@ -1,18 +1,42 @@
 package com.example.trial;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+//@RestController
+@Controller
 public class HelloController {
 
-	String[] name = {"orion","artemis","uranos","herios","iris"};
-	String[] mails = {"orion@star.gmobb.jp","artemis@star.gmobb.jp","uranos@star.gmobb.jp","herios@star.gmobb.jp","iris@star.gmobb.jp"};
+//	String[] name = {"orion","artemis","uranos","herios","iris"};
+//	String[] mails = {"orion@star.gmobb.jp","artemis@star.gmobb.jp","uranos@star.gmobb.jp","herios@star.gmobb.jp","iris@star.gmobb.jp"};
+/*
 	@RequestMapping("/{id}")
 	public DataObject index(@PathVariable int id) {
 		return new DataObject(id,name[id],mails[id]);
 //		return "nekoneko";
+	}
+*/
+/*
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "current data");
+		DataObject obj = new DataObject(123,"orion","orion@star.gmobb.jp");
+		mav.addObject("object", obj);
+		return mav;
+	}
+*/
+/*
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+*/
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		return mav;
 	}
 }
 
